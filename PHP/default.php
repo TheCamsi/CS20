98bfe131b47828db822d7c4838695223c5502330
TheCamsi <cameron.silvia@tufts.edu>
2024-03-26 19:45:47 -0400

    Create default.php

---
+++ PHP/default.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Practice</title>
+</head>
+<body>
+    <?php 
+        $n = $_GET('n');
+        //just wrote echo 15 times in a row
+        for($i = 0; $i < 15; $i++) {
+            echo $i . " x " . $n . " = " . $i * $n;
+        }
+    ?>
+</body>
+</html>
